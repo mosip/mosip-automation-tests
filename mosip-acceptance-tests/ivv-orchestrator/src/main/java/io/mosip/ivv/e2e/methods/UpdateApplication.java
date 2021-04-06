6a2578756ee0f26b0b2b15cb131f5e99e37e027d
AlokTechno <alok.tiwari@technoforte.co.in>
2021-04-06 17:15:36 +0530

    pre-registration missing function added

---
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/UpdateApplication.java
@@ -0,0 +1,19 @@
+package io.mosip.ivv.e2e.methods;
+
+import org.apache.log4j.Logger;
+import org.testng.Reporter;
+import io.mosip.ivv.core.base.StepInterface;
+import io.mosip.ivv.core.exceptions.RigInternalError;
+import io.mosip.ivv.orchestrator.BaseTestCaseUtil;
+
+public class UpdateApplication extends BaseTestCaseUtil implements StepInterface {
+	Logger logger = Logger.getLogger(UpdateApplication.class);
+
+	@Override
+	public void run() throws RigInternalError {
+		for (String resDataPath : residentTemplatePaths.keySet()) {
+			Reporter.log("<b><u>" + "UpdateApplication testCase </u></b>");
+			packetUtility.updateApplication(resDataPath, residentPathsPrid, contextKey);
+		}
+	}
+}
