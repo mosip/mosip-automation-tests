56a7d486fd3f91ab4ab4a7cd92cf6b479aef7b9b
SanathKumarV60 <75748160+SanathKumarV60@users.noreply.github.com>
2021-04-15 20:10:53 +0530

    fixed issues

---
+++ mosip-packet-creator/src/main/java/io/mosip/test/packetcreator/mosippacketcreator/controller/PrintController.java
@@ -0,0 +1,135 @@
+package io.mosip.test.packetcreator.mosippacketcreator.controller;
+
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import java.io.ByteArrayInputStream;
+//import java.util.HashMap;
+//import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.mosip.kernel.core.websub.spi.SubscriptionClient;
+import io.mosip.kernel.websub.api.annotation.PreAuthenticateContentAndVerifyIntent;
+//import io.mosip.kernel.websub.api.annotation.PreAuthenticateContentAndVerifyIntent;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
+import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
+import io.mosip.test.packetcreator.mosippacketcreator.dto.webSubEventModel;
+import io.mosip.test.packetcreator.mosippacketcreator.service.CryptoCoreUtil;
+
+
+
+
+import io.swagger.annotations.Api;
+
+@Api(value = "PrintController", description = "REST APIs for Websub subscription client")
+@RequestMapping(value = "/print")
+@RestController
+public class PrintController {
+
+	@Autowired
+	SubscriptionClient<SubscriptionChangeRequest,UnsubscriptionRequest, SubscriptionChangeResponse> sb; 
+	@Autowired
+	CryptoCoreUtil cryptoCoreUtil;
+	
+//	@Autowired
+//	PrintService printService;
+	
+	@Value("${mosip.test.print.topic}")
+	private String topic;
+
+	@Value("${mosip.test.print.event.secret}")
+	private String websubSecret;
+
+	@Value("${mosip.test.temp}")
+	private String tempPath;
+	
+	private static final Logger logger = LoggerFactory.getLogger(preRegController.class);
+	   
+
+	 @PostMapping(value = "/print/callback",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	 @PreAuthenticateContentAndVerifyIntent(secret = "${mosip.test.print.event.secret}",
+	 		callback = "/print/print/callback",topic = "${mosip.test.print.topic}")
+	 	public ResponseEntity<String>  printPost(@RequestBody webSubEventModel  eventModel) throws Exception {
+	
+		 logger.info("Print callback fired");
+		 String credential = eventModel.getEvent().getData().get("credential").toString();
+		 String ecryptionPin = eventModel.getEvent().getData().get("protectionKey").toString();
+		 String decodedCrdential = cryptoCoreUtil.decrypt(credential);
+		 logger.info(decodedCrdential);
+		 saveCreds(decodedCrdential);
+		 return new ResponseEntity<>("successfully printed", HttpStatus.OK);
+	 }
+	
+	 private void saveCreds(String creds) {
+		File credsPath = new File(tempPath + "/creds/");
+		if (!credsPath.exists()){
+			credsPath.mkdirs();
+		}
+		JSONObject jsonCred = new JSONObject(creds);
+		String fileName = credsPath.getAbsolutePath() + "/"+ jsonCred.getJSONObject("credentialSubject").getString("UIN") + ".cred";
+		try { 
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));		
+			writer.write(creds);
+			writer.close();
+
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		    
+		
+	 }
+	 private String getSignature(String sign, String crdential) {
+			String signHeader = sign.split("\\.")[0];
+			String signData = sign.split("\\.")[2];
+			String signature = signHeader + "." + crdential + "." + signData;
+			return signature;
+		}
+	 
+	
+	 @PostMapping(value = "/print/register/{callbackurl}")
+	 public @ResponseBody String registerPrintSubscription(@RequestBody String request,
+			 @RequestParam("callbackurl") String cbUrl,
+			 @RequestParam("webSubHubUrl") String webSubHubUrl) {
+	
+		 //String websubSecret = "{cipher}29ef73e366406ea1e7ac1d43e8d96002c3bd814a8b8cde9a961d897f2dadede5";
+		 //String topic="mpartner-default-print/CREDENTIAL_ISSUED";
+		 
+		 SubscriptionChangeRequest subscriptionRequest = new SubscriptionChangeRequest();
+		 subscriptionRequest.setCallbackURL(cbUrl);
+		 subscriptionRequest.setHubURL(webSubHubUrl + "/hub");
+		 subscriptionRequest.setSecret(websubSecret);
+		 subscriptionRequest.setTopic(topic);
+		 SubscriptionChangeResponse resp =  sb.subscribe(subscriptionRequest);
+		 return resp.getTopic();
+		 //return request;
+		 
+	 }
+		 
+	
+}
+
+
---
+++ mosip-packet-creator/src/main/java/io/mosip/test/packetcreator/mosippacketcreator/controller/preRegController.java
@@ -0,0 +1,279 @@
+package io.mosip.test.packetcreator.mosippacketcreator.controller;
+
+
+
+import org.mosip.dataprovider.util.DataProviderConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mosip.test.packetcreator.mosippacketcreator.dto.AppointmentDto;
+import io.mosip.test.packetcreator.mosippacketcreator.dto.PreRegisterRequestDto;
+import io.mosip.test.packetcreator.mosippacketcreator.service.PacketSyncService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "preRegController", description = "REST APIs for Pre Registration")
+@RequestMapping(value = "/prereg")
+@RestController
+public class preRegController {
+
+	  @Value("${mosip.test.persona.configpath}")
+		private String personaConfigPath;
+	  
+	  @Autowired
+	    PacketSyncService packetSyncService;
+
+	   private static final Logger logger = LoggerFactory.getLogger(preRegController.class);
+	   
+	@ApiOperation(value = "Request for OTP for the given Persona and channel {mail | phone}", response = String.class)
+		
+	@PostMapping(value = "/otp/request/{to}") 
+    public @ResponseBody String requestOtp(@RequestBody PreRegisterRequestDto preRegisterRequestDto, @PathVariable("to") String to,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.requestOtp(preRegisterRequestDto.getPersonaFilePath(), to, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("requestOtp", ex);
+    	}
+    	return "{Failed}";
+    }
+    
+	@ApiOperation(value = "Verify the sent OTP for the given Persona and channel {mail | phone}", response = String.class)
+	
+    @PostMapping(value = "/otp/verify/{to}")
+    public @ResponseBody String verifyOtp(@RequestBody PreRegisterRequestDto preRegisterRequestDto,@PathVariable("to") String to,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.verifyOtp(preRegisterRequestDto.getPersonaFilePath().get(0), to, null, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("verifyOtp", ex);
+    	}
+    	return "{Failed}";
+    }
+
+	@ApiOperation(value = "Post Application for a given Persona", response = String.class)
+
+    @PostMapping(value = "/application/")
+    public @ResponseBody String preRegisterResident(@RequestBody PreRegisterRequestDto preRegisterRequestDto,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.preRegisterResident(preRegisterRequestDto.getPersonaFilePath(), contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("registerResident", ex);
+    	}
+    	return "{Failed}";
+    }
+	@ApiOperation(value = "Update Application for a given PreRegID with updated Persona", response = String.class)
+	@PutMapping(value = "/application/{preregid}")
+    public @ResponseBody String updateResidentApplication(
+    		@RequestBody PreRegisterRequestDto preRegisterRequestDto,
+    		@PathVariable("preregid") String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.updateResidentApplication(preRegisterRequestDto.getPersonaFilePath().get(0),preregId, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("registerResident", ex);
+    	}
+    	return "{Failed}";
+    }
+
+	
+	@ApiOperation(value = "Retrive all Applications for the logged in user or for a given pre-registraion-Id", response = String.class)
+
+	@GetMapping(value = "/application/all/")
+    public @ResponseBody String preRegisterGetApplications(
+    		@RequestParam(name="preregId",required = false) String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	//28602756053278
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.preRegisterGetApplications(null,preregId, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("registerResident", ex);
+    	}
+    	return "{Failed}";
+    }
+	@ApiOperation(value = "Retrive all Booked Applications for the logged in user or for a given pre-registraion-Id", response = String.class)
+
+    @GetMapping(value = "/application/booked/")
+    public @ResponseBody String preRegisterGetApplicationsBooked(
+    		@RequestParam(name="preregId",required = false) String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.preRegisterGetApplications("Booked",preregId, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("registerResident", ex);
+    	}
+    	return "{Failed}";
+    }
+
+    /*
+     * Book first nn th available slot
+     */
+	@ApiOperation(value = "Book n'th Available slot for a given pre-registraion-Id", response = String.class)
+
+    @PostMapping(value = "/appointment/available/{preregid}/{nthSlot}")
+    public @ResponseBody String bookAppointment(@PathVariable("preregid") String preregId,@PathVariable("nthSlot") int  nthSlot,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.bookAppointment(preregId,nthSlot, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("bookAppointment", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+
+	@ApiOperation(value = "Book Specific slot (by date & time slot) for a given pre-registraion-Id", response = String.class)
+
+    @PostMapping(value = "/appointment/{preregid}")
+    public @ResponseBody String bookSpecificAppointment(@PathVariable("preregid") String preregId,
+    		@RequestBody AppointmentDto appointmentDto,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.bookSpecificAppointment(preregId,appointmentDto, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("bookAppointment", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+
+	@ApiOperation(value = "Get All Available slots", response = String.class)
+
+    @GetMapping(value = "/appointment/available/")
+    public @ResponseBody String getAvailableAppointments(@RequestParam(name="contextKey",required = true) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.getAvailableAppointments(contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("getAvailableAppointments", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+
+    /*
+     * Try to book a specified slot 
+     */
+	@ApiOperation(value = "Book n'th slot for a given pre-registraion-Id. If bookOnHoliday -> true ? then dont skip holidays", response = String.class)
+
+	@PostMapping(value = "/appointment/{preregid}/{nthSlot}/{bookOnHolidays}")
+    public @ResponseBody String bookAppointmentSpecified(@PathVariable("preregid") String preregId,
+    		@PathVariable("nthSlot") int  nthSlot,
+    		@PathVariable("bookOnHolidays") boolean bookOnHolidays,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.bookAppointmentSlot(preregId,nthSlot, bookOnHolidays,contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("bookAppointment", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+
+	@ApiOperation(value = "Upload documents for a given pre-registration-Id from specified persona", response = String.class)
+
+    @PostMapping(value = "/documents/{preregid}")
+    public @ResponseBody String uploadDocuments(@RequestBody PreRegisterRequestDto preRegisterRequestDto,@PathVariable("preregid") String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.uploadDocuments(preRegisterRequestDto.getPersonaFilePath().get(0),preregId, contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("uploadDocuments", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+    
+	@ApiOperation(value = "Cancel appointment for a given pre-registration-Id", response = String.class)
+
+    @PostMapping(value = "/appointment/cancel/{preregid}")
+    public @ResponseBody String cancelAppointment(
+    		@RequestBody AppointmentDto appointmentDto,
+    		@PathVariable("preregid") String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+
+    	try{    	
+    		if(personaConfigPath !=null && !personaConfigPath.equals("")) {
+    			DataProviderConstants.RESOURCE = personaConfigPath;
+    		}
+    		return packetSyncService.cancelAppointment(preregId, appointmentDto,contextKey);
+    	
+    	} catch (Exception ex){
+             logger.error("bookAppointment", ex);
+    	}
+    	return "{\"Failed\"}";
+    }
+	@ApiOperation(value = "Delete Applications for a given pre-registration-Id", response = String.class)
+
+    @DeleteMapping(value = "/application/{preregid}")
+    public @ResponseBody String deleteApplication(
+    		@PathVariable("preregid") String preregId,
+    		@RequestParam(name="contextKey",required = false) String contextKey) {
+    	
+    	return packetSyncService.deleteApplication(preregId, contextKey);
+    	
+    }
+    
+}
