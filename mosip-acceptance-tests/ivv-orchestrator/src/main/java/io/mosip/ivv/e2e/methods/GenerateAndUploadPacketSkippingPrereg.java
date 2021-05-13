38f3c63c2aa7368aa16f9c6047332b2bd57884a6
AlokTechno <alok.tiwari@technoforte.co.in>
2021-05-13 17:50:43 +0530

    assertion added for packet sync failure issue

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/GenerateAndUploadPacketSkippingPrereg.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/GenerateAndUploadPacketSkippingPrereg.java
@@ -1,5 +1,7 @@
 package io.mosip.ivv.e2e.methods;
 
+import org.apache.commons.lang.StringUtils;
+
 import io.mosip.ivv.core.base.StepInterface;
 import io.mosip.ivv.core.exceptions.RigInternalError;
 import io.mosip.ivv.orchestrator.BaseTestCaseUtil;
@@ -8,14 +10,21 @@ public class GenerateAndUploadPacketSkippingPrereg extends BaseTestCaseUtil impl
 
 	@Override
 	public void run() throws RigInternalError {
-
+		String responseStatus = "success";
+		if (step.getParameters() != null && !step.getParameters().isEmpty() && step.getParameters().size() == 1) { // failed
+			responseStatus = step.getParameters().get(0);
+			if (StringUtils.isBlank(responseStatus) || !responseStatus.equalsIgnoreCase("failed"))
+				throw new RigInternalError("Parameter : [" + responseStatus + "] is not allowed");
+		}
 		for (String resDataPath : residentTemplatePaths.keySet()) {
 			String rid = packetUtility.generateAndUploadPacketSkippingPrereg(residentTemplatePaths.get(resDataPath),
-					resDataPath,contextInuse);
-			pridsAndRids.put("0", rid);
-			ridPersonaPath.put(rid, resDataPath);
+					resDataPath, contextInuse, responseStatus);
+			if (rid != null) {
+				pridsAndRids.put("0", rid);
+				ridPersonaPath.put(rid, resDataPath);
+			}
 		}
-		
+
 	}
 
 }
