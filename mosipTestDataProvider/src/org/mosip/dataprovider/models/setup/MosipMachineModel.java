c4575f19e70194caa191f428b0546c08316aa626
AlokTechno <alok.tiwari@technoforte.co.in>
2021-10-25 18:07:21 +0530

    prereg issue fixed and few jsonIgnore property added

--- mosipTestDataProvider/src/org/mosip/dataprovider/models/setup/MosipMachineModel.java
+++ mosipTestDataProvider/src/org/mosip/dataprovider/models/setup/MosipMachineModel.java
@@ -2,9 +2,12 @@ package org.mosip.dataprovider.models.setup;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties({"createdBy","createdDateTime","updatedBy","updatedDateTime","isDeleted","deletedDateTime","zone","machineTypeName","mapStatus"})
 public class MosipMachineModel implements Serializable{
 
 	 private static final long serialVersionUID = 1L;
