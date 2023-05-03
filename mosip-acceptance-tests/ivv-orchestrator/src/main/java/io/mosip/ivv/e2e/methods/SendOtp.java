0a8460deba9ef009965c513b60aaeb67c067074d
Sohan Kumar Dey <72375959+Sohandey@users.noreply.github.com>
2023-05-03 16:42:31 +0530

    added dynamic otp support for prereg

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/SendOtp.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/SendOtp.java
@@ -1,9 +1,14 @@
 package io.mosip.ivv.e2e.methods;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import io.mosip.ivv.core.base.StepInterface;
 import io.mosip.ivv.core.exceptions.RigInternalError;
 import io.mosip.ivv.orchestrator.BaseTestCaseUtil;
+import io.mosip.kernel.util.ConfigManager;
 
 public class SendOtp extends BaseTestCaseUtil implements StepInterface {
 	Logger logger = Logger.getLogger(SendOtp.class);
@@ -12,21 +17,23 @@ public class SendOtp extends BaseTestCaseUtil implements StepInterface {
 	public void run() throws RigInternalError {
 		Boolean isForChildPacket = false;
 		String emailOrPhone = "test.automation@gmail.com";
+
 		if (step.getParameters().isEmpty()) {
 			// emailOrPhone =step.getParameters().get(0);
 			for (String resDataPath : step.getScenario().getResidentTemplatePaths().keySet()) {
-				packetUtility.requestOtp(resDataPath, step.getScenario().getCurrentStep(), emailOrPhone,step);
+				packetUtility.requestOtp(resDataPath, step.getScenario().getCurrentStep(), emailOrPhone, step);
 			}
 		} else if (!step.getParameters().isEmpty() && step.getParameters().size() == 1
 				&& !step.getParameters().get(0).startsWith("$$")) { // used for child packet processing
 			isForChildPacket = Boolean.parseBoolean(step.getParameters().get(0));
 			if (isForChildPacket && !step.getScenario().getGeneratedResidentData().isEmpty())
-				packetUtility.requestOtp(step.getScenario().getGeneratedResidentData().get(0), step.getScenario().getCurrentStep(), emailOrPhone,step);
+				packetUtility.requestOtp(step.getScenario().getGeneratedResidentData().get(0),
+						step.getScenario().getCurrentStep(), emailOrPhone, step);
 		} else {
-			String personaFilePath = step.getParameters().get(0);   //"$$var=e2e_sendOtp($$personaFilePath)"
+			String personaFilePath = step.getParameters().get(0); // "$$var=e2e_sendOtp($$personaFilePath)"
 			if (personaFilePath.startsWith("$$")) {
 				personaFilePath = step.getScenario().getVariables().get(personaFilePath);
-				packetUtility.requestOtp(personaFilePath, step.getScenario().getCurrentStep(), emailOrPhone,step);
+				packetUtility.requestOtp(personaFilePath, step.getScenario().getCurrentStep(), emailOrPhone, step);
 			}
 		}
 	}
