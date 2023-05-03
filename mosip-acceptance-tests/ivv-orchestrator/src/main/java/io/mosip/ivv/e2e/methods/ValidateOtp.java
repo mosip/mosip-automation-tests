39f8c316d044b94568a9c0f20334e455383bf830
Sohan Kumar Dey <72375959+Sohandey@users.noreply.github.com>
2023-05-03 18:03:57 +0530

    MOSIP-27288

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/ValidateOtp.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/ValidateOtp.java
@@ -19,7 +19,6 @@ public class ValidateOtp extends BaseTestCaseUtil implements StepInterface {
 	public void run() throws RigInternalError {
 
 		Boolean isForChildPacket = false;
-		String emailOrPhone = "test.automation@gmail.com";
 		Properties kernelprops=ConfigManager.propsKernel;
 		String emailId=kernelprops.getProperty("usePreConfiguredEmail");
 		
@@ -27,7 +26,7 @@ public class ValidateOtp extends BaseTestCaseUtil implements StepInterface {
 		if (step.getParameters().isEmpty()) {
 			// emailOrPhone =step.getParameters().get(0);
 			for (String resDataPath : step.getScenario().getResidentTemplatePaths().keySet()) {
-				packetUtility.verifyOtp(resDataPath, step.getScenario().getCurrentStep(), emailOrPhone, step,
+				packetUtility.verifyOtp(resDataPath, step.getScenario().getCurrentStep(), emailId, step,
 						MockSMTPListener.getOtp(1, emailId));
 			}
 		} else if (!step.getParameters().isEmpty() && step.getParameters().size() == 1
@@ -35,13 +34,13 @@ public class ValidateOtp extends BaseTestCaseUtil implements StepInterface {
 			isForChildPacket = Boolean.parseBoolean(step.getParameters().get(0));
 			if (isForChildPacket && !step.getScenario().getGeneratedResidentData().isEmpty())
 				packetUtility.verifyOtp(step.getScenario().getGeneratedResidentData().get(0),
-						step.getScenario().getCurrentStep(), emailOrPhone, step,
+						step.getScenario().getCurrentStep(), emailId, step,
 						MockSMTPListener.getOtp(1, emailId));
 		} else {
 			String personaFilePath = step.getParameters().get(0); // "$$var=e2e_validateOtp($$personaFilePath)"
 			if (personaFilePath.startsWith("$$")) {
 				personaFilePath = step.getScenario().getVariables().get(personaFilePath);
-				packetUtility.verifyOtp(personaFilePath, step.getScenario().getCurrentStep(), emailOrPhone, step,
+				packetUtility.verifyOtp(personaFilePath, step.getScenario().getCurrentStep(), emailId, step,
 						MockSMTPListener.getOtp(10, emailId));
 			}
 		}
