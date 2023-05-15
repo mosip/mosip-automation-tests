b4b2fabc1a7a569200ec4e8bbe27138759350c49
Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>
2023-05-15 13:25:27 +0530

    AdditionalReqId fix

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/GetAdditionalReqId.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/ivv/e2e/methods/GetAdditionalReqId.java
@@ -3,48 +3,51 @@ package io.mosip.ivv.e2e.methods;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.collections4.map.HashedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.json.JSONObject;
 
 import io.mosip.ivv.core.base.StepInterface;
 import io.mosip.ivv.core.exceptions.RigInternalError;
 import io.mosip.ivv.orchestrator.BaseTestCaseUtil;
 import io.mosip.testrunner.MockSMTPListener;
-import io.restassured.response.Response;
 
-
-public class GetAdditionalReqId extends BaseTestCaseUtil implements StepInterface {  //  $$additionalReqId=e2e_getAdditionalReqId(10)
+public class GetAdditionalReqId extends BaseTestCaseUtil implements StepInterface { // $$additionalReqId=e2e_getAdditionalReqId(10)
 
 	Logger logger = Logger.getLogger(GetAdditionalReqId.class);
+
 	@Override
 	public void run() throws RigInternalError {
 		int counter = 0;
 		int repeats = 10; // taking from dsl argument
+		String email = null;
 		if (!step.getParameters().isEmpty() && step.getParameters().size() > 0)
-			repeats = Integer.parseInt(step.getParameters().get(0));
+			email = step.getParameters().get(0) + "@mosip.io";
 		String url = baseUrl + props.getProperty("getAdditionalInfoReqId");
-		Map<Object, Object> m=new HashMap<Object, Object>();
-		String additonalInfoRequestId=null;
+		Map<Object, Object> m = new HashMap<Object, Object>();
+		String additonalInfoRequestId = null;
 		while (counter < repeats) {
-			m=MockSMTPListener.emailNotificationMapS;
-			if(m.get("dsl_getAddReqInfo@mosip.io")!=null) {
-			String html=(String) m.get("dsl_getAddReqInfo@mosip.io");
-			 StringUtils.substringBetween(html, "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1");
-			//String arr[]=html.split("AdditionalInfoRequestId","AdditionalInfoRequestId".indexOf(str) ;
-			//System.out.println(arr);
-			
-			logger.info("*******Checking the email for AdditionalInfoReqId...*******");
-			//Response response = getRequest(url, "Get addtionalInfoRequestId");
-			//String additonalInfoRequestId = response.getBody().asString();
-			 additonalInfoRequestId= StringUtils.substringBetween(html, "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1").trim()+"-BIOMETRIC_CORRECTION-1";
-			//additonalInfoRequestId= StringUtils.substringBetween(html, "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1");
-			
+			m = MockSMTPListener.emailNotificationMapS;
+			if (m.get(email) != null) {
+				String html = (String) m.get(email);
+				StringUtils.substringBetween(html, "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1");
+				// String
+				// arr[]=html.split("AdditionalInfoRequestId","AdditionalInfoRequestId".indexOf(str)
+				// ;
+				// System.out.println(arr);
+
+				logger.info("*******Checking the email for AdditionalInfoReqId...*******");
+				// Response response = getRequest(url, "Get addtionalInfoRequestId");
+				// String additonalInfoRequestId = response.getBody().asString();
+				additonalInfoRequestId = StringUtils
+						.substringBetween(html, "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1").trim()
+						+ "-BIOMETRIC_CORRECTION-1";
+				// additonalInfoRequestId= StringUtils.substringBetween(html,
+				// "AdditionalInfoRequestId", "-BIOMETRIC_CORRECTION-1");
+
 			}
 			if (additonalInfoRequestId != null && !additonalInfoRequestId.isEmpty()
 					&& !additonalInfoRequestId.equals("{Failed}")) {
-			
+
 				logger.info("AdditionalInfoReqId retrieved: " + additonalInfoRequestId);
 				if (step.getOutVarName() != null)
 					step.getScenario().getVariables().put(step.getOutVarName(), additonalInfoRequestId);
