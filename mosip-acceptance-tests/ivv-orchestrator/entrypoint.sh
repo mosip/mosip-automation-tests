54d13d4fe7a32ad334640639541f2de0c8fd3841
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-06-26 15:50:27 +0530

    [MOSIP-27662] updated mount path in dsl-orchestrator entrypoint.sh

--- mosip-acceptance-tests/ivv-orchestrator/entrypoint.sh
+++ mosip-acceptance-tests/ivv-orchestrator/entrypoint.sh
@@ -6,7 +6,7 @@ for file in $files; do
   domain=$(printenv installation-domain)
   dir_name="$mountPathForScenario/scenarios/";
   file_name=$(basename $file | sed "s/env/$domain/g")
-  echo "DIRECTORY and MOUNT PATH : $mountPathForScenario  $file_name";
+  echo "DIRECTORY and MOUNT PATH : $dir_name/$file_name";
   mkdir -p $dir_name;
   cp "$file" "$dir_name/$file_name";
 done
