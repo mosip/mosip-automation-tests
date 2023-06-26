596c9963bc7dd082078c208b844125adc7217c33
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-06-26 15:37:07 +0530

    [MOSIP-27662] Added Packetcreator data and scenario sheet to dockers

--- mosip-packet-creator/entrypoint.sh
+++ mosip-packet-creator/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+files=$( find /tmp/profile_resource -type f )
+for file in $files; do
+  dir_name=$(dirname $file | sed 's/\/tmp\///g');
+  file_name=$(basename $file)
+  echo "DIRECTORY : $dir_name and MOUNT PATH : $mountPath/$dir_name";
+  mkdir -p "$mountPath/$dir_name";
+  cp "$file" "$mountPath/$dir_name/$file_name";
+done
+
 java --version
 java -Dfile.encoding=UTF-8  -jar mosip-packet-creator-*-SNAPSHOT.jar --spring.config.location=./config/application.properties
