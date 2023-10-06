d543f92b03fa615b618c90f16c36f7e4742c3207
Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>
2023-10-06 18:05:25 +0530

    MOSIP-29702

--- mosipTestDataProvider/src/io/mosip/testrig/dslrig/dataprovider/DateOfBirthProvider.java
+++ mosipTestDataProvider/src/io/mosip/testrig/dslrig/dataprovider/DateOfBirthProvider.java
@@ -8,11 +8,12 @@ import com.ibm.icu.text.SimpleDateFormat;
 
 import io.mosip.testrig.dslrig.dataprovider.util.DataProviderConstants;
 import io.mosip.testrig.dslrig.dataprovider.util.ResidentAttribute;
+import io.mosip.testrig.dslrig.dataprovider.variables.VariableManager;
 
 public class DateOfBirthProvider {
 
 	private static Random rand = new Random();
-
+	
 	public static String generateDob(int minAge, int maxAge) {
 		int offset = rand.nextInt(maxAge - minAge) + minAge;
 		Calendar calendar = Calendar.getInstance();
@@ -22,16 +23,23 @@ public class DateOfBirthProvider {
 		return formatter.format(dob);
 	}
 
-	public static String generate(ResidentAttribute ageAttribute) {
-		if (ageAttribute == ResidentAttribute.RA_Adult)
+	public static String generate(ResidentAttribute ageAttribute,String contextKey) {
+		if (ageAttribute == ResidentAttribute.RA_Adult) {
+			 VariableManager.setVariableValue(contextKey, "AGE_GROUP", "ADULT");
 			return generateDob(DataProviderConstants.Age_Adult_Min_Age, DataProviderConstants.Age_Senior_Citizen_Min_Age);
-		else if (ageAttribute == ResidentAttribute.RA_Minor)
+		}
+		else if (ageAttribute == ResidentAttribute.RA_Minor) {
+			 VariableManager.setVariableValue(contextKey, "AGE_GROUP", "CHILD");
 			return generateDob(DataProviderConstants.Age_Minor_Min_Age, DataProviderConstants.Age_Adult_Min_Age);
-		else if (ageAttribute == ResidentAttribute.RA_Senior)
+		}
+		else if (ageAttribute == ResidentAttribute.RA_Senior) {
+			 VariableManager.setVariableValue(contextKey, "AGE_GROUP", "SENIOR_CITIZEN");
 			return generateDob(DataProviderConstants.Age_Senior_Citizen_Min_Age, 100);
-		else if (ageAttribute == ResidentAttribute.RA_Infant)
+		}
+		else if (ageAttribute == ResidentAttribute.RA_Infant) {
+			 VariableManager.setVariableValue(contextKey, "AGE_GROUP", "CHILD");
 			return generateDob(0, 5);
-
+		}
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
 		return formatter.format(new Date());
 	}
