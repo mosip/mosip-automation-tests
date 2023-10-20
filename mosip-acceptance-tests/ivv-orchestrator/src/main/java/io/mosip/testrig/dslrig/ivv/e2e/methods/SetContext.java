15a0bd5b2700c8a333d6ecf0dd5201fe12b198ec
Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>
2023-10-20 11:30:31 +0530

    MOSIP-29836
    Signed-off-by: Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/testrig/dslrig/ivv/e2e/methods/SetContext.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/testrig/dslrig/ivv/e2e/methods/SetContext.java
@@ -15,7 +15,7 @@ import io.mosip.testrig.dslrig.ivv.orchestrator.PacketUtility;
 
 public class SetContext extends BaseTestCaseUtil implements StepInterface {
 	static Logger logger = Logger.getLogger(SetContext.class);
-	
+
 	static {
 		if (ConfigManager.IsDebugEnabled())
 			logger.setLevel(Level.ALL);
@@ -27,23 +27,26 @@ public class SetContext extends BaseTestCaseUtil implements StepInterface {
 	public void run() throws RigInternalError {
 		constantIntializer();
 		String contextKeyValue = "dev_context";
-	
+
 		String userAndMachineDetailParam = null;
 		String mosipVersion = null;
 		boolean generatePrivateKey = Boolean.FALSE;
 		String status = null;
-		String negative="valid";
+		String negative = "valid";
 		boolean invalidCertFlag = Boolean.FALSE;
-		String consent="";
-		boolean changeSupervisorNameToDiffCase=Boolean.FALSE;
-		//neeha scenario = step.getScenario().getId() + ":" + step.getScenario().getDescription();
+		String consent = "";
+		boolean changeSupervisorNameToDiffCase = Boolean.FALSE;
+		String invalidEncryptedHashFlag = "";
+		String invalidCheckSum = "";
+		// neeha scenario = step.getScenario().getId() + ":" +
+		// step.getScenario().getDescription();
 		HashMap<String, String> map = new HashMap<String, String>();
 		HashMap<String, String> dummyholder = new HashMap<String, String>();
 		if (step.getParameters() == null || step.getParameters().isEmpty() || step.getParameters().size() < 1) {
 			logger.warn("SetContext Arugemnt is  Missing : Please pass the argument from DSL sheet");
 		} else {
-			contextKeyValue = step.getParameters().get(0); 
-			contextKeyValue=System.getProperty("env.user")+"_context";
+			contextKeyValue = step.getParameters().get(0);
+			contextKeyValue = System.getProperty("env.user") + "_context";
 			// step.getScenario().getContextInuse()put("contextKey",contextKeyValue );
 			step.getScenario().getCurrentStep().put(contextKeyValue, "true");
 			step.getScenario().getCurrentStep().clear();
@@ -53,7 +56,7 @@ public class SetContext extends BaseTestCaseUtil implements StepInterface {
 				if (!(value.equalsIgnoreCase("-1")) && value.contains("@@"))
 					userAndMachineDetailParam = value;
 				else if (value.startsWith("$$")) {
-					
+
 					map = step.getScenario().getVariables();
 				}
 			}
@@ -67,27 +70,36 @@ public class SetContext extends BaseTestCaseUtil implements StepInterface {
 
 			if (step.getParameters().size() > 4) // deactivate
 				status = step.getParameters().get(4);
-			if (step.getParameters().size() > 5) //  for negative operator and supervisor 
+			if (step.getParameters().size() > 5) // for negative operator and supervisor
 				negative = step.getParameters().get(5);
-			
-			if(step.getParameters().size() == 5 && step.getParameters().get(4).contains("true"))
+
+			if (step.getParameters().size() == 5 && step.getParameters().get(4).contains("true"))
 				invalidCertFlag = Boolean.parseBoolean(step.getParameters().get(4));
-			
-			// consent value either "Y" or "N"
-			if(step.getParameters().size() == 6 && (step.getParameters().get(5).contains("yes") || step.getParameters().get(5).contains("no")))
+
+			// consent value either "yes" or "no"
+			if (step.getParameters().size() == 6
+					&& (step.getParameters().get(5).contains("yes") || step.getParameters().get(5).contains("no")))
 				consent = step.getParameters().get(5);
-			
-			
+
 			// supervisorIDFlag
-			if(step.getParameters().size() > 6 && step.getParameters().get(6).contains("true"))
+			if (step.getParameters().size() > 6 && step.getParameters().get(6).contains("true"))
 				changeSupervisorNameToDiffCase = Boolean.parseBoolean(step.getParameters().get(6));
+
+			// encryptedHashFlag
+			if (step.getParameters().size() > 6 && step.getParameters().get(7).contains("invalidEncryptedHash"))
+				invalidEncryptedHashFlag = step.getParameters().get(7);
+			// checksumFlag
+			if (step.getParameters().size() == 9 && step.getParameters().get(8).contains("invalidCheckSum"))
+				invalidCheckSum = step.getParameters().get(8);
+
 		}
-		
+
 		if (userAndMachineDetailParam != null)
 			packetUtility.createContexts(contextKeyValue, userAndMachineDetailParam, mosipVersion, generatePrivateKey,
-					status, BaseTestCase.ApplnURI + "/",step);
-		else if(map != null)
-			packetUtility.createContexts(negative,contextKeyValue, map, mosipVersion, generatePrivateKey, status,
-					BaseTestCase.ApplnURI + "/",step,invalidCertFlag,consent,changeSupervisorNameToDiffCase);
+					status, BaseTestCase.ApplnURI + "/", step);
+		else if (map != null)
+			packetUtility.createContexts(negative, contextKeyValue, map, mosipVersion, generatePrivateKey, status,
+					BaseTestCase.ApplnURI + "/", step, invalidCertFlag, consent, changeSupervisorNameToDiffCase,
+					invalidEncryptedHashFlag, invalidCheckSum);
 	}
 }
