0735ddeba1e01a12a8565ddda156d4eb3c5e90ad
Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>
2023-11-30 18:50:18 +0530

    MOSIP-30517
    Signed-off-by: Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>

--- mosip-packet-creator/src/main/resources/dockersupport/centralized/mosip-packet-creator/run_centralized_packet_creator.bat
+++ mosip-packet-creator/src/main/resources/dockersupport/centralized/mosip-packet-creator/run_centralized_packet_creator.bat
@@ -5,4 +5,4 @@ chdir /d  D:\centralized\mosip-packet-creator
 del /f PacketUtilityRunlog.txt
 cls
 
-java -Xss8m  -Dfile.encoding=UTF-8 -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9999,suspend=n -jar dslrig-packetcreator-1.2.1-SNAPSHOT.jar --spring.config.location=file:///D:\centralized\mosip-packet-creator\config\application.properties>>D:\centralized\mosip-packet-creator\PacketUtilityRunlog.txt
+java -Xss8m  -Dfile.encoding=UTF-8 -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9999,suspend=n -jar dslrig-packetcreator-1.2.1-develop-SNAPSHOT.jar --spring.config.location=file:///D:\centralized\mosip-packet-creator\config\application.properties>>D:\centralized\mosip-packet-creator\PacketUtilityRunlog.txt
