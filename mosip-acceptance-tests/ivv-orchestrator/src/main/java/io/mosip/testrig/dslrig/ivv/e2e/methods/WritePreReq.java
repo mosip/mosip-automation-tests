0cc328411689c6e59b3f95e4830afd7e67e8ade8
Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>
2023-12-27 22:29:32 +0530

    MOSIP-30926
    Signed-off-by: Pankaj Godiyal <pankaj.godiyal@technoforte.co.in>

--- mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/testrig/dslrig/ivv/e2e/methods/WritePreReq.java
+++ mosip-acceptance-tests/ivv-orchestrator/src/main/java/io/mosip/testrig/dslrig/ivv/e2e/methods/WritePreReq.java
@@ -1,27 +1,21 @@
 package io.mosip.testrig.dslrig.ivv.e2e.methods;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.testng.Reporter;
-
 import io.mosip.testrig.apirig.kernel.util.ConfigManager;
 import io.mosip.testrig.apirig.service.BaseTestCase;
 import io.mosip.testrig.dslrig.ivv.core.base.StepInterface;
+import io.mosip.testrig.dslrig.ivv.core.exceptions.RigInternalError;
 import io.mosip.testrig.dslrig.ivv.orchestrator.BaseTestCaseUtil;
-import io.mosip.testrig.dslrig.ivv.orchestrator.PacketUtility;
 import io.mosip.testrig.dslrig.ivv.orchestrator.TestRunner;
 
 public class WritePreReq extends BaseTestCaseUtil implements StepInterface {
 	static Logger logger = Logger.getLogger(WritePreReq.class);
-	
+
 	static {
 		if (ConfigManager.IsDebugEnabled())
 			logger.setLevel(Level.ALL);
@@ -30,7 +24,7 @@ public class WritePreReq extends BaseTestCaseUtil implements StepInterface {
 	}
 
 	@Override
-	public void run() {
+	public void run() throws RigInternalError {
 
 		String value = null;
 		String appendedkey = null;
@@ -38,55 +32,36 @@ public class WritePreReq extends BaseTestCaseUtil implements StepInterface {
 		Reporter.log("==========STEP ====== WritePreReq ");
 		if (step.getParameters() == null || step.getParameters().isEmpty() || step.getParameters().size() < 1) {
 			logger.warn("PreRequisite Arugemnt is  Missing : Please pass the argument from DSL sheet");
-		} else if(step.getParameters().size() >= 1) {
+		} else if (step.getParameters().size() >= 1) {
 			value = step.getParameters().get(0);
 
 			if (value.startsWith("$$")) {
 				map = step.getScenario().getVariables();
 			}
 		}
-		if(step.getParameters().size() >= 2) {appendedkey=step.getParameters().get(1);
-		map.put("appendedkey", appendedkey);
+		if (step.getParameters().size() >= 2) {
+			appendedkey = step.getParameters().get(1);
+			map.put("appendedkey", appendedkey);
 		}
 		Properties props = new Properties();
-		  Properties kernelprops=ConfigManager.propsKernel;
-
-	      props.putAll(kernelprops);
-	      props.putAll(map);
-	      
-	      FileOutputStream outputStrem = null;
+		Properties kernelprops = ConfigManager.propsKernel;
 		try {
-			
+		props.putAll(kernelprops);
+		props.putAll(map);
+
 			String path = (TestRunner.getExternalResourcePath() + "/config/" + BaseTestCase.environment + "_prereqdata_"
 					+ appendedkey + ".properties");
-			/*
-			 * File file = new File(path);
-			 * 
-			 * if (file.createNewFile()) {
-			 * 
-			 * Reporter.log("File has been created at this path : " +path); } else {
-			 * 
-			 * Reporter.log("File already exists at the path : " + path); } outputStrem =
-			 * new FileOutputStream(path);
-			 * 
-			 * props.store(outputStrem, "This is path where file is created" + path);
-			 */
 			HashMap<String, String> propertiesMap = new HashMap<String, String>();
 			for (Entry<Object, Object> entry : props.entrySet()) {
 				propertiesMap.put((String) entry.getKey(), (String) entry.getValue());
 			}
-			
 			prereqDataSet.put(path, propertiesMap);
 			if (ConfigManager.IsDebugEnabled())
-			 Reporter.log(props.toString());
-	
-		} 
-		/*
-		 * catch (IOException e) { logger.error(e.getMessage()); }
-		 */
-		finally {
-			PacketUtility.closeOutputStream(outputStrem);
+				Reporter.log(props.toString());
+		} catch (Exception e) {
+			this.hasError = true;
+			logger.error(e.getMessage());
+			throw new RigInternalError("WritePreRequisite Data is not returned properly");
 		}
-							
 	}
 }
