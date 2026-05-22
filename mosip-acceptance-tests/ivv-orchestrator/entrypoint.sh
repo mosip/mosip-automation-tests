1f37c8d18e9e3668c316eea010cc0f7bdacdf731
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2026-05-22 14:08:27 +0530

    code rabbit
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- mosip-acceptance-tests/ivv-orchestrator/entrypoint.sh
+++ mosip-acceptance-tests/ivv-orchestrator/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-files=$( find /tmp/scenarios -type f )
-
-if [[ $DEFAULT_SCENARIOS == "true" ]]; then
-  for file in $files; do
+# Legacy: copy bundled JSON scenario sheets into the mount when present (useExternalScenarioSheet=yes).
+# Default Gherkin flow uses build_files/config/scenarios.feature; /tmp/scenarios is optional.
+if [[ "${DEFAULT_SCENARIOS}" == "true" && -d /tmp/scenarios ]]; then
+  while IFS= read -r -d '' file; do
     domain=$(printenv installation-domain)
-    dir_name="$mountPathForScenario/scenarios/";
-    file_name=$(basename $file | sed "s/env/$domain/g")
-    echo "DIRECTORY and MOUNT PATH : $dir_name/$file_name";
-    mkdir -p $dir_name;
-    cp "$file" "$dir_name/$file_name";
-  done
+    dir_name="${mountPathForScenario}/scenarios/"
+    file_name=$(basename "$file" | sed "s/env/$domain/g")
+    echo "DIRECTORY and MOUNT PATH : ${dir_name}${file_name}"
+    mkdir -p "$dir_name"
+    cp "$file" "${dir_name}${file_name}"
+  done < <(find /tmp/scenarios -type f -print0)
 fi
 
 ## Run DSL Orchestrator
