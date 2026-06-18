ca9a7055774613f66551276e3b86bdf845f15541
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2026-06-18 16:03:24 +0530

    MOSIP-45191
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- mosipTestDataProvider/src/io/mosip/testrig/dslrig/dataprovider/PacketTemplateProvider.java
+++ mosipTestDataProvider/src/io/mosip/testrig/dslrig/dataprovider/PacketTemplateProvider.java
@@ -1054,11 +1054,11 @@ public class PacketTemplateProvider {
 			}
 			if (processDynamicFields(s, identity, resident, contextKey))
 				continue;
-			if (s.getFieldCategory().equals("evidence") && s.getId().equals("nrcId") ) {
+			if (s.getId().contains("nrcId")) {
 				identity.put(s.getId(),resident.getNrcId().getNrcId());
 				continue;
 			}
-			if (s.getFieldCategory().equals("pvt") || s.getFieldCategory().equals("kyc")) {
+			if (true) {
 				String primaryValue = "";
 				String secValue = "";
 				if (VariableManager.getVariableValue(contextKey, "name") != null
@@ -1380,11 +1380,11 @@ public class PacketTemplateProvider {
 				continue;
 			}
 			
-			if (s.getFieldCategory().equals("evidence") && s.getId().equals("nrcId") ) {
+			if (s.getFieldCategory() != null && s.getFieldCategory().equals("evidence") && s.getId().equals("nrcId") ) {
 				identity.put(s.getId(),resident.getNrcId().getNrcId());
 				continue;
 			}
-			if (s.getFieldCategory().equals("pvt") || s.getFieldCategory().equals("kyc")) {
+			if (s.getFieldCategory() != null && (s.getFieldCategory().equals("pvt") || s.getFieldCategory().equals("kyc"))) {
 				String primaryValue = "";
 				String secValue = "";
 				if (VariableManager.getVariableValue(contextKey, "name") != null
